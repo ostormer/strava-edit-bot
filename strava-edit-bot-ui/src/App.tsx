f6453ae613b39b39fc505ce5e4ff04792ec9c224
Oskar Størmer <oskar.stormer@gmail.com>
2026-03-28 23:27:47 +0100

    Basic vite+react install

---
+++ strava-edit-bot-ui/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+
+export default function App() {
+  return (
+    <div className="p-8">
+      <h1>Strava Edit Bot</h1>
+      <Button>Test Button</Button>
+    </div>
+  )
+}
