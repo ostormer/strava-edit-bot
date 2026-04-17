9c73c2092e0ae55b5e2a907ee87a4918a06258ff
Oskar Størmer <oskar.stormer@gmail.com>
2026-04-17 13:29:45 +0200

    Cleanup, delete old activity concept

--- StravaEditBotApi/Data/AppDbContext.cs
+++ StravaEditBotApi/Data/AppDbContext.cs
@@ -7,6 +7,5 @@ namespace StravaEditBotApi.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options)
     : IdentityDbContext<AppUser>(options)
 {
-    public DbSet<Activity> Activities => Set<Activity>();
     public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
 }
---
+++ StravaEditBotApi/Migrations/20260417112159_RemoveActivityTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StravaEditBotApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveActivityTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Activities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ActivitySport = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Distance = table.Column<double>(type: "float", nullable: false),
+                    ElapsedTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                });
+        }
+    }
+}
